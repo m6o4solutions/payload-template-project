311e48af2cce44ad7ba7264a727465e6aaa4c912
Michael Owuor <michael.owuor@gmail.com>
2025-06-27 23:47:33 +0300

    feat: simplify user and media collections

--- src/payload/collections/media/schema.ts
+++ src/payload/collections/media/schema.ts
@@ -1,7 +1,3 @@
-import { FixedToolbarFeature, InlineToolbarFeature, lexicalEditor } from "@payloadcms/richtext-lexical";
-
-import { isAuthenticated, isPublic } from "@/payload/access/access-control";
-
 import type { CollectionConfig } from "payload";
 
 const Media: CollectionConfig = {
@@ -10,47 +6,16 @@ const Media: CollectionConfig = {
 		singular: "Media",
 		plural: "Media",
 	},
-	admin: {
-		defaultColumns: ["filename", "alt", "mimeType", "caption", "site"],
-		useAsTitle: "filename",
-	},
+
 	access: {
-		// authenticated users can create
-		create: isAuthenticated,
 		// anyone can read
-		read: isPublic,
-		// authenticated users can update
-		update: isAuthenticated,
-		// authenticated users can delete
-		delete: isAuthenticated,
+		read: () => true,
 	},
 	fields: [
 		{
 			name: "alt",
 			type: "text",
 			required: true,
-		},
-		{
-			name: "caption",
-			type: "richText",
-			editor: lexicalEditor({
-				features: ({ rootFeatures }) => {
-					return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()];
-				},
-			}),
-		},
-		{
-			name: "site",
-			type: "relationship",
-			relationTo: "sites",
-			required: true,
-			// if user is not admin, set the site by default to the first site
-			// that they have access to.
-			defaultValue: ({ user }: { user: { roles: string[]; sites?: string[] } }) => {
-				if (!user.roles.includes("admin") && user.sites?.[0]) {
-					return user.sites[0];
-				}
-			},
 		},
 	],
 	upload: {
--- src/payload/collections/users/hooks/populate-full-name.ts
+++
@@ -1,7 +0,0 @@
-import type { FieldHook } from "payload";
-
-const populateFullName: FieldHook = async ({ data }) => {
-	return `${data?.firstName} ${data?.lastName}`;
-};
-
-export { populateFullName };
