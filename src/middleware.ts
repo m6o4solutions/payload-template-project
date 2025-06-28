3fedcee2940d3ebd4d1277feecfc6d0ea4657e39
Michael Owuor <michael.owuor@gmail.com>
2025-06-28 07:12:37 +0300

    feat: adds clerk authentication

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/(.*)", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+	if (!isPublicRoute(req)) {
+		await auth.protect();
+	}
+});
+
+export const config = {
+	matcher: [
+		// skip Next.js internals and all static files, unless found in search params
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// always run for api routes
+		"/(api|trpc)(.*)",
+	],
+};
