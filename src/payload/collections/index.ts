1cd0bdad4843fe5350c1f1dd72c4f8d30c55be47
Michael Owuor <michael.owuor@gmail.com>
2025-06-30 22:46:39 +0300

    refactor: change collections export to use const declaration (#7)

--- src/payload/collections/index.ts
+++ src/payload/collections/index.ts
@@ -2,4 +2,6 @@ import { Customers } from "@/payload/collections/customers/schema";
 import { Media } from "@/payload/collections/media/schema";
 import { Users } from "@/payload/collections/users/schema";
 
-export const collections = [Media, Users, Customers];
+const collections = [Media, Users, Customers];
+
+export { collections };
