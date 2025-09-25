928d21ccd001a799b9f6478a0e0ae8b254c2c9a7
Michael Owuor <michael.owuor@gmail.com>
2025-09-25 17:28:58 +0300

    feat: refactor layout and add theme provider

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { Montserrat } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+import { FooterServer as Footer } from "@/payload/blocks/globals/footer/server";
+import { HeaderServer as Header } from "@/payload/blocks/globals/header/server";
 
 import type { Metadata } from "next";
 
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-	title: "Payload CMS Basic Template",
+	title: "Payload Basic Template",
 	description: "Template to get started with Next.js, Payload 3.0 CMS and Tailwind CSS.",
 };
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+const RootLayout = async (props: { children: ReactNode }) => {
 	const { children } = props;
 
 	return (
-		<html lang="en">
-			<body>
-				<main>{children}</main>
+		<html lang="en" suppressHydrationWarning>
+			<body className={cn("flex h-screen flex-col", montserrat.className)}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<header>
+						<Header />
+					</header>
+
+					<main>{children}</main>
+
+					<footer className="mt-auto">
+						<Footer />
+					</footer>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
-}
+};
+
+export default RootLayout;
