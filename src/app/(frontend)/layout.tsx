5712be23a0e9591d1a4428c1fc59a089946598ed
Michael Owuor <michael.owuor@gmail.com>
2025-09-25 15:10:03 +0300

    feat: enhance template with new components and improve layout structure

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+
+import type { Metadata } from "next";
+
 import "@/styles/globals.css";
 
-export const metadata = {
-	description: "A blank template using Payload in a Next.js app.",
-	title: "Payload Blank Template",
+export const metadata: Metadata = {
+	title: "Payload CMS Basic Template",
+	description: "Template to get started with Next.js, Payload 3.0 CMS and Tailwind CSS.",
 };
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/container";
+
 const HomePage = async () => {
-	return <div className="text-3xl font-bold underline">Welcome to your Payload + Next.js app!</div>;
+	return (
+		<Container>
+			<article>
+				<section className="pt-36 pb-36">
+					<h1 className="font-header text-4xl font-bold tracking-wide text-sky-800">
+						Payload Basic Template
+					</h1>
+
+					<p className="my-5">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, cum
+						reprehenderit. Doloremque corrupti aliquid nihil magnam impedit, quisquam
+						soluta? Expedita et dolorum quo laudantium odio esse, distinctio nulla hic ea.
+					</p>
+
+					<Button
+						className="rounded-lg font-semibold uppercase"
+						variant="default"
+						asChild
+					>
+						<Link href="/admin" target="_blank">
+							Admin Area
+						</Link>
+					</Button>
+				</section>
+			</article>
+		</Container>
+	);
 };
 
 export default HomePage;
