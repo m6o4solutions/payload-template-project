7ffc7f5407bc1bc20519ffac8d0fbe19ee4cdcfe
Michael Owuor <michael.owuor@gmail.com>
2025-09-25 20:18:12 +0300

    fix: allow images from specific external paths and block all others (#8)
    * chore(release): 1.1.0 [skip ci]
    
    # [1.1.0](https://github.com/m6o4solutions/payload-basic-template-project/compare/v1.0.0...v1.1.0) (2025-09-25)
    
    ### Features
    
    * enhance template with new components and improve layout structure ([#5](https://github.com/m6o4solutions/payload-basic-template-project/issues/5)) ([9716d29](https://github.com/m6o4solutions/payload-basic-template-project/commit/9716d2958144dc31465851cfd92f6634cd923c17))
    
    * chore(release): 1.1.0-rc.1 [skip ci]
    
    # [1.1.0-rc.1](https://github.com/m6o4solutions/payload-basic-template-project/compare/v1.0.0...v1.1.0-rc.1) (2025-09-25)
    
    ### Features
    
    * add access control functions for user authentication ([70e29c6](https://github.com/m6o4solutions/payload-basic-template-project/commit/70e29c67d8819036083a7a4fbc286f71757ca8cf))
    * add name and photo fields to User interface and schema ([ca1875c](https://github.com/m6o4solutions/payload-basic-template-project/commit/ca1875c85137a981b5922f65a6213b8c828156dc))
    * enhance media schema with caption and size options ([b1d6305](https://github.com/m6o4solutions/payload-basic-template-project/commit/b1d6305c8c7107f1f28cbc9c12dfa64f472fc222))
    * enhance template with new components and improve layout structure ([#5](https://github.com/m6o4solutions/payload-basic-template-project/issues/5)) ([9716d29](https://github.com/m6o4solutions/payload-basic-template-project/commit/9716d2958144dc31465851cfd92f6634cd923c17))
    * integrate Uploadthing storage plugin and enhance media schema ([6508380](https://github.com/m6o4solutions/payload-basic-template-project/commit/6508380744f9fd20639f2c956c4b821499b6fc43))
    
    * chore(release): 1.2.0-rc.1 [skip ci]
    
    # [1.2.0-rc.1](https://github.com/m6o4solutions/payload-basic-template-project/compare/v1.1.0...v1.2.0-rc.1) (2025-09-25)
    
    ### Features
    
    * add access control functions for user authentication ([70e29c6](https://github.com/m6o4solutions/payload-basic-template-project/commit/70e29c67d8819036083a7a4fbc286f71757ca8cf))
    * add name and photo fields to User interface and schema ([ca1875c](https://github.com/m6o4solutions/payload-basic-template-project/commit/ca1875c85137a981b5922f65a6213b8c828156dc))
    * enhance media schema with caption and size options ([b1d6305](https://github.com/m6o4solutions/payload-basic-template-project/commit/b1d6305c8c7107f1f28cbc9c12dfa64f472fc222))
    * integrate Uploadthing storage plugin and enhance media schema ([6508380](https://github.com/m6o4solutions/payload-basic-template-project/commit/6508380744f9fd20639f2c956c4b821499b6fc43))
    
    * chore: add staging branch to semantic release configuration
    
    * fix: allow images from specific external paths and block all others
    
    * chore(release): 1.2.0-dev.1 [skip ci]
    
    # [1.2.0-dev.1](https://github.com/m6o4solutions/payload-basic-template-project/compare/v1.1.0...v1.2.0-dev.1) (2025-09-25)
    
    ### Bug Fixes
    
    * allow images from specific external paths and block all others ([2bc803f](https://github.com/m6o4solutions/payload-basic-template-project/commit/2bc803f7f7ef42d49f6f0c956e80bf4d8892fbc6))
    
    ### Features
    
    * add access control functions for user authentication ([70e29c6](https://github.com/m6o4solutions/payload-basic-template-project/commit/70e29c67d8819036083a7a4fbc286f71757ca8cf))
    * add name and photo fields to User interface and schema ([ca1875c](https://github.com/m6o4solutions/payload-basic-template-project/commit/ca1875c85137a981b5922f65a6213b8c828156dc))
    * enhance media schema with caption and size options ([b1d6305](https://github.com/m6o4solutions/payload-basic-template-project/commit/b1d6305c8c7107f1f28cbc9c12dfa64f472fc222))
    * integrate Uploadthing storage plugin and enhance media schema ([6508380](https://github.com/m6o4solutions/payload-basic-template-project/commit/6508380744f9fd20639f2c956c4b821499b6fc43))
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,20 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 
-const createRemotePattern = (url) => {
-	const { protocol, hostname, port } = new URL(url);
-
-	return {
-		protocol: protocol.replace(":", ""),
-		hostname,
-		port: hostname === "localhost" ? port : undefined,
-	};
-};
-
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		qualities: [25, 50, 75],
-		remotePatterns: [createRemotePattern(NEXT_PUBLIC_SERVER_URL)],
+		qualities: [25, 50, 75, 100],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname,
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "3000",
+			},
+		],
 	},
 	output: "standalone",
 	webpack: (webpackConfig) => {
