fb99f31addfafda9eba70f6954f7b43d7ebbef29
Michael Owuor <michael.owuor@gmail.com>
2025-10-15 14:00:07 +0300

    chore: update message on not found page

--- src/app/(web)/not-found.tsx
+++ src/app/(web)/not-found.tsx
@@ -10,7 +10,7 @@ const NotFound = () => {
 				<h1 className="text-primary mb-4 text-6xl font-bold">404</h1>
 				<h2 className="text-primary mb-4 text-2xl font-semibold">Page Not Found</h2>
 				<p className="text-foreground mb-8">
-					Oops! The page you are looking for does not exist or has been moved.
+					Oops! The page you are looking for either does not exist or has been moved.
 				</p>
 
 				<Button className="rounded-lg font-semibold uppercase" variant="default" asChild>
